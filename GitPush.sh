be6bfa049aa7ebf5a21103ba099ed7f97189f505
Your Name <you@example.com>
2019-02-01 13:21:47 +0800

    update

--- GitPush.sh
+++ GitPush.sh
@@ -1,3 +1,8 @@
+echo "***************************"
+echo "****** Push Your Code *****"
+echo "***** ZhaoKaifeng.com *****"
+echo "* github.com/somebodyblog *"
+echo "***************************"
 git add .
 git commit -m "update"
 git push
